b8ccfa52f253395c4c8166ed76d1cee809bf08ba
Valden <espeut.clement@gmail.com>
2016-05-28 13:14:08 +0200

    Explorateur de carte buggé mais qui marcheé

---
+++ loadCard.js
@@ -0,0 +1,81 @@
+				
+function loadCard(tag, deck, id)
+{
+				// Enlever les classe de la carte du back pour pouvoir les recharger en fonction de ce qu'il y a dans le json
+			  	$(tag).removeClass().addClass("card");
+
+			  	// Si la carte à un type, le charger
+			  	if (deck[id].type)
+			  	{
+			  		$(tag).addClass("suit"+deck[id].type);
+			  	}
+
+
+			  	// Charger le texte de la carte
+			  	var txt = deck[id].content;
+
+			  	var joueursTmp = joueurs.slice();
+			  	var random = 0;
+			  	// Patterns !
+			  	// Définition des parterns : {lePattern}
+			  	var personne = [];
+			  	for(var i =0; i<3; i++)
+			  	{
+			  		random = Math.floor((Math.random() * (joueursTmp.length)));
+
+			  		personne[i] = joueursTmp[random];
+			  		joueursTmp.splice(random,1);
+
+			  	}
+
+			  	txt = txt.replace(/{j1}/g, personne[0]);
+			  	txt = txt.replace(/{j2}/g, personne[1]);
+			  	txt = txt.replace(/{j3}/g, personne[2]);
+			  	txt = txt.replace(/{b}/g, "<i class='fa fa-beer' aria-hidden='true'></i>");
+
+			  	// Remplacer verres random
+				var result = txt.match(/{b\d+-\d+}/g);
+				if (result != null)
+				{
+					for (var m = 0; m<result.length; m++) {
+						var data = result[m];
+
+						var n = data.match(/(\d+)/g);
+						n[0] = parseInt(n[0]);
+						n[1] = parseInt(n[1]);
+						random = Math.floor((Math.random() * (n[1]-n[0]+1))+n[0]);
+
+						var beer = "";
+
+						if (random <= 5)
+						{
+							for (var i = 0; i < random; i++)
+							{
+								beer = beer + "<i class='fa fa-beer' aria-hidden='true'></i>";
+							}
+						}
+						else
+						{
+							beer = "" + random + " <i class='fa fa-beer' aria-hidden='true'></i>";
+						}
+					    txt = txt.replace(data,beer)
+					}
+				}
+
+				// Remplacer choix multiple :
+
+				var result = txt.match(/{[^}]*\|[^}]*}/g);
+				if (result != null)
+				{
+					for (var m = 0; m<result.length; m++) {
+						var data = result[m];
+
+						var n = data.match(/[^{\|}]+/g)
+						random = Math.floor((Math.random() * (n.length)));
+					    txt = txt.replace(data,n[random]);
+					}
+				}
+
+
+			  	$(tag + " p").html(txt);
+}
